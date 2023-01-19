4371407166c14d391a4ed1cef0ef8a6f2b63bee6
M_Brito <38845517+trtroks@users.noreply.github.com>
2023-01-19 13:19:15 -0300

    Update heap_sort.py

--- heap_sort.py
+++ heap_sort.py
@@ -1,14 +1,35 @@
-from heapq import heappop, heappush
+import time
+inicio=time.time()
+tamanho=100
+def heapify(arr, n, i): 
+    largest = i  # Initialize largest as root 
+    l = 2 * i + 1  # left = 2*i + 1 
+    r = 2 * i + 2  # right = 2*i + 2 
+  
+    if l < n and arr[i] < arr[l]: 
+        largest = l 
+    if r < n and arr[largest] < arr[r]: 
+        largest = r   
+    if largest != i: 
+        (arr[i], arr[largest]) = (arr[largest], arr[i])
+        heapify(arr, n, largest)
+        
+def heapSort(arr): 
+    n = len(arr)  
+    for i in range(n // 2 - 1, -1, -1): 
+        heapify(arr, n, i) 
+    for i in range(n - 1, 0, -1): 
+        (arr[i], arr[0]) = (arr[0], arr[i]) 
+        heapify(arr, i, 0)
+        
+# arr = [12, 11, 13, 5, 6, 7, ]
 
-def heapsort(list1):
-     heap = []
-     for ele in list1:
-         heappush(heap, ele)
-
-     sort = []
-
-     #  os elementos são levantados na pilha
-     while heap:
-         sort.append(heappop(heap))
-
-     return sort
+# crescente=[crescente for crescente in range(tamanho)]
+# decrescente=[tamanho-decrescente for decrescente in range(tamanho)]
+# # print(crescente)
+# heapSort(crescente) 
+# n = len(crescente) 
+# print('Sorted array is')  
+# print(crescente)
+# fim=time.time()-inicio
+# print(f'\nTempo de final: {fim:.5f}')
