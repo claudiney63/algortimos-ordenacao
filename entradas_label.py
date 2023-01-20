c21a42919d8a724bd8a4df29bb9cd4ae22a9354a
M_Brito <38845517+trtroks@users.noreply.github.com>
2023-01-19 23:36:29 -0300

    Update entradas_label.py

--- entradas_label.py
+++ entradas_label.py
@@ -7,37 +7,107 @@ def entradas_label():
     print('Quantidade de Entradas: ')
     print('1. 100;\n2. 250;\n3. 500;\n4. 1.000;\n5. 5.000;\n6. 30.000;\n7. 50.000;\n8. 100.000;\n9. 150.000;\n0. 200.000.')
 
+    with open("100numbers.txt", "r") as arquivo:
+        cemnumbers=[]
+        for line in arquivo.readlines():
+            fields = line.split(',')
+        for i in range(len(fields)):
+            cemnumbers.append(int(fields[i]))
+
+    with open("250numbers.txt", "r") as arquivo2:
+        cclnumbers=[]
+        for line in arquivo2.readlines():
+            fields = line.split(',')
+        for i in range(len(fields)):
+            cclnumbers.append(int(fields[i]))
+            
+    with open("500numbers.txt", "r") as arquivo3:
+        cccccnumbers=[]
+        for line in arquivo3.readlines():
+            fields = line.split(',')
+        for i in range(len(fields)):
+            cccccnumbers.append(int(fields[i]))
+            
+    with open("1000numbers.txt", "r") as arquivo:
+        knumbers=[]
+        for line in arquivo.readlines():
+            fields = line.split(',')
+        for i in range(len(fields)):
+            knumbers.append(int(fields[i]))
+            
+    with open("5000numbers.txt", "r") as arquivo:
+        cincoknumbers=[]
+        for line in arquivo.readlines():
+            fields = line.split(',')
+        for i in range(len(fields)):
+            cincoknumbers.append(int(fields[i]))
+
+    with open("30000numbers.txt", "r") as arquivo:
+        trintaknumbers=[]
+        for line in arquivo.readlines():
+            fields = line.split(',')
+        for i in range(len(fields)):
+            trintaknumbers.append(int(fields[i]))
+            
+    with open("50000numbers.txt", "r") as arquivo:
+        cinquentaknumbers=[]
+        for line in arquivo.readlines():
+            fields = line.split(',')
+        for i in range(len(fields)):
+            cinquentaknumbers.append(int(fields[i]))
+            
+    with open("100000numbers.txt", "r") as arquivo:
+        cemknumbers=[]
+        for line in arquivo.readlines():
+            fields = line.split(',')
+        for i in range(len(fields)):
+            cemknumbers.append(int(fields[i]))
+            
+    with open("150000numbers.txt", "r") as arquivo:
+        centoecinquentaknumbers=[]
+        for line in arquivo.readlines():
+            fields = line.split(',')
+        for i in range(len(fields)):
+            centoecinquentaknumbers.append(int(fields[i]))
+
+    with open("200000numbers.txt", "r") as arquivo:
+        duzentosknumbers=[]
+        for line in arquivo.readlines():
+            fields = line.split(',')
+        for i in range(len(fields)):
+            duzentosknumbers.append(int(fields[i]))
+            
     escolha = int(input('Escolha: '))
     if (escolhatype==1):
         if (escolha == 1):
-            saida_da_entrada = [random.randrange(1, 100) for i in range(100)]
+            saida_da_entrada = cemnumbers
             return saida_da_entrada
         elif(escolha==2):
-            saida_da_entrada = [random.randrange(1,250) for i in range(250)]
+            saida_da_entrada = cclnumbers
             return saida_da_entrada
         elif(escolha==3):
-            saida_da_entrada=[random.randrange(1,500) for i in range(500)]
+            saida_da_entrada=cccccnumbers
             return saida_da_entrada
         elif(escolha == 4):
-            saida_da_entrada = [random.randrange(1, 1000) for i in range(1000)]
+            saida_da_entrada = knumbers
             return saida_da_entrada
         elif(escolha == 5):
-            saida_da_entrada = [random.randrange(1, 5000) for i in range(5000)]
+            saida_da_entrada = cincoknumbers
             return saida_da_entrada
         elif(escolha == 6):
-            saida_da_entrada = [random.randrange(1, 30000) for i in range(30000)]
+            saida_da_entrada = trintaknumbers
             return saida_da_entrada
         elif(escolha == 7):
-            saida_da_entrada = [random.randrange(1, 50000) for i in range(50000)]
+            saida_da_entrada = cinquentaknumbers
             return saida_da_entrada
         elif(escolha == 8):
-            saida_da_entrada = [random.randrange(1, 100000) for i in range(100000)]
+            saida_da_entrada = cemknumbers
             return saida_da_entrada
         elif(escolha == 9):
-            saida_da_entrada = [random.randrange(1, 150000) for i in range(150000)]
+            saida_da_entrada = centoecinquentaknumbers
             return saida_da_entrada
         elif(escolha == 0):
-            saida_da_entrada = [random.randrange(1, 200000) for i in range(200000)]
+            saida_da_entrada = duzentosknumbers
             return saida_da_entrada
     elif(escolhatype==2):
         if (escolha == 1):
