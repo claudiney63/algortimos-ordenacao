61e30da384634dd1a56fd6cfa76c3cdcbc0b3d43
M_Brito <38845517+trtroks@users.noreply.github.com>
2023-01-19 22:11:07 -0300

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -2,9 +2,9 @@
 #coloca o pivô em sua posição exata e coloca os
 #elementos menores à esquerda do pivot e maiores
 #elementos à direita do pivot. 
-  
+
 def partition (a, start, end):  
-    i = (start - 1)  
+    i = (start - 1)
     pivot = a[end] # elemento pivot  
       
     for j in range(start, end):  
@@ -15,24 +15,28 @@ def partition (a, start, end):
       
     a[i+1], a[end] = a[end], a[i+1]  
   
-    return (i + 1)  
+    return (i + 1)
+
       
 # função para implementar classificação rápida 
 def quick(a, start, end): # a[] = array a ser classificado, start = início índice, end = finalização índice
-    if (start < end):  
+    if (start < end):
         p = partition(a, start, end) #p está particionado no índice
         quick(a, start, p - 1)  
-        quick(a, p + 1, end)  
+        quick(a, p + 1, end)
+#     return a
   
           
-def printArr(a): # Função para imprimir o array  
-    for i in range(len(a)):  
-        print (a[i], end = " ")  
+# def printArr(a): # Função para imprimir o array  
+#    for i in range(len(a)):  
+#        print (a[i], end = " ")  
   
       
-a = [68, 13, 1, 49, 58]  
-print("Before sorting array elements are - ")  
-printArr(a)  
-quick(a, 0, len(a)-1)  
-print("\nAfter sorting array elements are - ")  
-printArr(a)  
+#a =[100000-i for i in range(100000)]
+# print("Before sorting array elements are - ")  
+# printArr(a)
+# print(sys.getrecursionlimit())
+# quick(a, 0, len(a)-1)  
+ #print("\nAfter sorting array elements are - ")  
+# printArr(a)  
+
