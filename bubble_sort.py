c0091884adc1bf0bd90db76acabe071fb7ca579d
M_Brito <38845517+trtroks@users.noreply.github.com>
2023-01-30 11:39:19 -0300

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,11 +1,15 @@
 # Criando uma função de classificação em bolhas
+comp=0
 def bubble_sort(list1):  
     # Loop externo para percorrer toda a lista 
+    global comp
     for i in range(len(list1)-1):  
-        for j in range(len(list1)-1):  
-            if(list1[j]>list1[j+1]):  
+        for j in range(len(list1)-1):
+            comp+=1
+            if(list1[j]>list1[j+1]):
                 temp = list1[j]  
                 list1[j] = list1[j+1]  
                 list1[j+1] = temp  
-
+    print("comparações=")
+    print(comp)
     return list1
