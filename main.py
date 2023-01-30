c3a6ae6a75d70f4a760324e4cc3f51535d25eef9
M_Brito <38845517+trtroks@users.noreply.github.com>
2023-01-30 11:41:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import hibrid2
 import sys
 sys.setrecursionlimit(200050)
 if __name__ == '__main__':
+    comp=0
 
     escolha = algoritimo_label.algoritimo_label()
 
@@ -58,8 +59,9 @@ if __name__ == '__main__':
         for i in range(0, 1):
             
             escolha_aux = escolha_ord.copy()
-            merge_sort.merge_sort(escolha_aux, 0, len(escolha_aux) - 1)
-            
+            comp=merge_sort.merge_sort(escolha_aux, 0, len(escolha_aux) - 1)
+            print("numero de comparações=")
+            print(comp)
             print("\nArray Ordenado: ", escolha_aux)
 
             fim = time.time() - inicio
@@ -96,38 +98,18 @@ if __name__ == '__main__':
         for i in range(0, 1):
             
             escolha_aux = escolha_ord.copy()
-            quick_sort.quick(escolha_aux, 0, len(escolha_aux) - 1)
-            
+            comp=quick_sort.quick(escolha_aux, 0, len(escolha_aux) - 1)
+            print("numero de comparações=")
+            print(comp)
             print("\nArray Ordenado: ", escolha_aux)
-
             fim = time.time() - inicio
             soma_tempo += fim
 
             print('\nAlgoritimo Quick Sort:')
             print(f'Entradas: N = {len(escolha_aux)}')
             print(f"Tempo Execucao: {(soma_tempo):.5f} s")
-    elif (escolha == 6): #Hybrid Merge + Insertion
-        escolha_ord = entradas_label.entradas_label()
-        inicio = time.time()
-        print("Array NAO ordenado: ", escolha_ord)
-        # Chamada função para ordenar
-        soma_tempo = 0
-        
-        for i in range(0, 1):
-            
-            escolha_aux = escolha_ord.copy()
-            hibrid.hibrid_merge_insertion_sort(escolha_aux, 0, len(escolha_aux))
-            
-            print("\nArray Ordenado: ", escolha_aux)
 
-            fim = time.time() - inicio
-            soma_tempo += fim
-
-        print('\nAlgoritimo Hybrid Merge + Insertion Sort:')
-        print(f'Entradas: N = {len(escolha_aux)}')
-        print(f"Tempo Execucao: {(soma_tempo):.5f} s")
-
-    elif (escolha == 7): #Hybrid Merge + Quick
+    elif (escolha == 6): #Hybrid Merge + Quick
         escolha_ord = entradas_label.entradas_label()
         inicio = time.time()
         print("Array NAO ordenado: ", escolha_ord)
